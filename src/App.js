148959bfe5046597b285e0364ba2404a6080e1db
Aftab6639 <Aftabhussain16639@gmail.com>
2024-02-29 16:55:23 +0500

    setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1 className="git-page">Main uses of Git and GitHub</h1>
+
+    </div>
+  );
+}
+
+export default App;
